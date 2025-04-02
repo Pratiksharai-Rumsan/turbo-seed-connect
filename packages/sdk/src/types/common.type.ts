0fb5e0f27ec36ecb61ea817293e2b700a739822a
Pratiksharai-rumsan <pratiksha.rai@rumsan.net>
2025-04-02 15:05:56 +0545

    define type in for the transport

---
+++ packages/sdk/src/types/common.type.ts
@@ -0,0 +1,7 @@
+export type CommonFields = {
+     id?: string;
+    cuid?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+    deletedAt?: Date 
+}
---
+++ packages/sdk/src/types/transport.type.ts
@@ -0,0 +1,67 @@
+import { CommonFields } from "./common.type";
+
+
+
+
+export enum TransportType {
+  SMTP = 'SMTP',
+  VOICE = 'VOICE',
+  API = 'API',
+  SES = 'SES',
+  ECHO = 'ECHO',
+}
+
+// export type Transport = {
+//   app?: string;
+//   name: string;
+//   type: TransportType;
+//   config: Record<string, any>;
+//   id?: number;
+//   cuid?: string;
+//   stats?: Record<string, any> | null;
+//   validationContent?: string;
+//   validationAddress?: string;
+//   createdAt?: Date;
+//   updatedAt?: Date | null;
+//   deletedAt?: Date | null;
+// };
+
+export type TransportConfig<T> = {
+  app?: string;
+  name: string;
+  type: TransportType;
+  config: T;
+  //id?: number;
+ // cuid?: string;
+  stats?: Record<string, any> | null;
+  //createdAt?: Date;
+  //updatedAt?: Date | null;
+  //deletedAt?: Date | null;
+};
+export type Transport<T> = TransportConfig<T> & CommonFields
+export type CreateAccount<T> = TransportConfig<T>
+export type EditTransport<T> = Partial<TransportConfig<T>>
+
+
+export type TransportSmtpConfig = {
+  host: string;
+  port?: number;
+  secure?: boolean;
+  username: string;
+  password: string;
+  defaultFrom?: string;
+  defaultSubject?: string;
+};
+
+export type TransportApiConfig = {
+  url: string;
+  method?: string;
+  headers?: Record<string, string>;
+  body?: Record<string, any>;
+  timeout?: number;
+};
+
+export type TransportEchoConfig = {
+  slack_url?: string;
+  slack_email?: string;
+};
