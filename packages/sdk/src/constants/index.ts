c9ff973c56ef67b8c1220a0dcdc1602fb34116c0
Pratiksharai-rumsan <pratiksha.rai@rumsan.net>
2025-04-04 11:51:15 +0545

    adjust file export

--- packages/sdk/src/constants/index.ts
+++ packages/sdk/src/constants/index.ts
@@ -1,1 +1,4 @@
+// Export all constant files from this directory
+// For example:
 export * from './events';
+// Add more exports for each constant file in this directory
--- packages/sdk/src/types/index.ts
+++ packages/sdk/src/types/index.ts
@@ -1,3 +1,7 @@
+// Export all type files from this directory
+// For example:
+
+// Add more exports for each type file in this directory
 export * from './interfaces';
 export * from './pagination.type';
 export * from './queue.type';
